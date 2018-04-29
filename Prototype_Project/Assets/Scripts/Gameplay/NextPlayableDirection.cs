9cab6f660a8aa15ab85a22b688eda853a82f641a
Tiryth <felipemilkewicz@gmail.com>
2018-04-28 21:33:24 -0300

    Juntado Brench e ajeitado animações
    Juntei minha brench com a master e ajustei as animações da primeira parte, acho que ficou ok, da pra melhorar bastante, mas eh bastante trabalho.
    Fiz um jeito onde posso jogar com os "tempos" das animações mais facil depois se precisar, por isso tem varias timelines
    Tive que mudar o script de chamar uma timeline nova para ela dar active pq as cameras estavam se batendo com todas ligadas.

--- Prototype_Project/Assets/Scripts/Gameplay/NextPlayableDirection.cs
+++ Prototype_Project/Assets/Scripts/Gameplay/NextPlayableDirection.cs
@@ -5,12 +5,18 @@ using UnityEngine.Playables;
 
 public class NextPlayableDirection : MonoBehaviour {
 
-	//playble director to play next
+    //playble director to play next
+    public GameObject director;
 	public PlayableDirector nextDirector;
 
 	//Starts when the GameObject is activated
 	void OnEnable(){
-		//plays the next playable director
+        //plays the next playable director
+        if (director != null)
+        {
+            director.SetActive(true);
+            gameObject.SetActive(false);
+        }
 		nextDirector.Play();
 		//destroys this gameobject parent (previous playable director)
 		Destroy(transform.parent.gameObject);
